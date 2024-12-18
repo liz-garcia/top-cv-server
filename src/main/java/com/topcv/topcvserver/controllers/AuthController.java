dffa82e8feb8ce9199bb2e8feefae999e6059de9
liz-garcia <lizgarciamarin@gmail.com>
2024-12-18 20:06:56 +0100

    Added JwtIssuer into Security package and AuthController.

--- src/main/java/com/topcv/topcvserver/controllers/AuthController.java
+++ src/main/java/com/topcv/topcvserver/controllers/AuthController.java
@@ -2,23 +2,28 @@ package com.topcv.topcvserver.controllers;
 
 import com.topcv.topcvserver.models.LoginRequest;
 import com.topcv.topcvserver.models.LoginResponse;
+import com.topcv.topcvserver.security.JwtIssuer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final JwtIssuer jwtIssuer;
+
     // Login endpoint
     @PostMapping("/api/auth/login")
-    public LoginResponse login(@RequestBody @Validated LoginRequest loginRequest) { // still need to set up validation
-        // TODO: Implement login logic using loginRequest
+    public LoginResponse login(@RequestBody @Validated LoginRequest request) { // still need to set up validation
+        var token = jwtIssuer.issue(1L, request.getEmail(), List.of("USER"));
 
         return LoginResponse.builder()
-                .accessToken("Bla bla bla")
+                .accessToken(token)
                 .build();
     }
 }
---
+++ src/main/java/com/topcv/topcvserver/security/JwtIssuer.java
@@ -0,0 +1,23 @@
+package com.topcv.topcvserver.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Component
+public class JwtIssuer {
+
+    public String issue(long userId, String email, List<String> roles) {
+        return JWT.create()
+                .withSubject(String.valueOf(userId))
+                .withExpiresAt(Instant.now().plus(Duration.of(1, ChronoUnit.DAYS))) // later change to minutes and use refreshToken to refresh it
+                .withClaim("email", email)
+                .withClaim("authorities", roles)
+                .sign(Algorithm.HMAC256("secret"));
+    }
+}
