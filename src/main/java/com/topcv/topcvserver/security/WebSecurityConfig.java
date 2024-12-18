902ef8454909a079649668da374d33f789c6e794
liz-garcia <lizgarciamarin@gmail.com>
2024-12-18 17:53:30 +0100

    Added initial configurations to ensure HttpRequests authorization.

--- src/main/java/com/topcv/topcvserver/security/WebSecurityConfig.java
+++ src/main/java/com/topcv/topcvserver/security/WebSecurityConfig.java
@@ -14,10 +14,15 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable) // temporary for initial setup
-                .csrf(AbstractHttpConfigurer::disable) // temporary for initial setup
+                .cors(AbstractHttpConfigurer::disable) // temporary for initial dev setup
+                .csrf(AbstractHttpConfigurer::disable) // temporary for initial dev setup
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless sessions
-                .formLogin(AbstractHttpConfigurer::disable); // Disable form login
+                .formLogin(AbstractHttpConfigurer::disable) // Disable form login
+                .securityMatcher("/**") // Ensure the filter chain to all requests. Could also be set to "/api/**"
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/").permitAll() // Allow public access to "/" without requiring authentication
+                        .anyRequest().authenticated() // Require authentication for all other routes
+                );
 
         return http.build();
     }
