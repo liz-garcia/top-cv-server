b0a08f51b539842b9e8c09493f023b5bf5c8e175
liz-garcia <lizgarciamarin@gmail.com>
2024-12-19 20:04:02 +0100

    Implmented JwtDecoder and JwtToPrincipalConverter.

--- src/main/java/com/topcv/topcvserver/security/WebSecurityConfig.java
+++ src/main/java/com/topcv/topcvserver/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.topcv.topcvserver.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,12 +8,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         http
                 .cors(AbstractHttpConfigurer::disable) // temporary for initial dev setup
                 .csrf(AbstractHttpConfigurer::disable) // temporary for initial dev setup
