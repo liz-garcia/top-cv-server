2881b2579e20ed58db8f6a9bf36c895aae2c73a1
liz-garcia <lizgarciamarin@gmail.com>
2024-12-19 09:26:38 +0100

    Initial config for secret key.

--- src/main/java/com/topcv/topcvserver/security/JwtIssuer.java
+++ src/main/java/com/topcv/topcvserver/security/JwtIssuer.java
@@ -2,6 +2,7 @@ package com.topcv.topcvserver.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -10,7 +11,10 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class JwtIssuer {
+
+    private final JwtProperties properties;
 
     public String issue(long userId, String email, List<String> roles) {
         return JWT.create()
@@ -18,6 +22,6 @@ public class JwtIssuer {
                 .withExpiresAt(Instant.now().plus(Duration.of(1, ChronoUnit.DAYS))) // later change to minutes and use refreshToken to refresh it
                 .withClaim("email", email)
                 .withClaim("authorities", roles)
-                .sign(Algorithm.HMAC256("secret"));
+                .sign(Algorithm.HMAC256(properties.getSecretKey()));
     }
 }
