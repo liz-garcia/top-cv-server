0b901d434297d494c6ae6add533fa36b3d73fc9e
liz-garcia <lizgarciamarin@gmail.com>
2025-01-29 13:46:26 +0100

    Refactored AuthController. Created AuthService.

--- src/main/java/com/topcv/topcvserver/controllers/AuthController.java
+++ src/main/java/com/topcv/topcvserver/controllers/AuthController.java
@@ -2,13 +2,8 @@ package com.topcv.topcvserver.controllers;
 
 import com.topcv.topcvserver.models.LoginRequest;
 import com.topcv.topcvserver.models.LoginResponse;
-import com.topcv.topcvserver.security.JwtIssuer;
-import com.topcv.topcvserver.security.UserPrincipal;
+import com.topcv.topcvserver.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,27 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final JwtIssuer jwtIssuer;
-    private final AuthenticationManager authenticationManager;
+    private final AuthService authService;
 
     // Login endpoint
     @PostMapping("/api/auth/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request) { // still need to set up validation
-        var authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        var principal = (UserPrincipal) authentication.getPrincipal();
-
-        var roles = principal.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .toList();
-
-        var token = jwtIssuer.issue(principal.getUserId(), principal.getEmail(), roles);
-
-        return LoginResponse.builder()
-                .accessToken(token)
-                .build();
+        return authService.attemptLogin(request.getEmail(), request.getPassword());
     }
 }
